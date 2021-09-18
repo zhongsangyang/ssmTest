4111688ac83e888ff3fd27f69a42f537fc25574f
zhongsangyang <5217079a>
2021-09-18 15:14:32 +0800

    chore(项目jar包spring版本升级):change spring.version from 4.0.0-->5.0.13Release
    2017实习->2021年工作学习git提交规范这里是body详细描述部分
    
    BREAKING CHANGE: before:RequestMapping()-->After:GetMapping()

--- src/main/java/gzfzkj/controller/UserController.java
+++ src/main/java/gzfzkj/controller/UserController.java
@@ -5,10 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -18,22 +16,22 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/user")
 public class UserController {
 
-        private static Logger logger=LoggerFactory.getLogger(UserController.class);
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
 
-        // /user/test?id=1
-        @RequestMapping(value = "/test",method = RequestMethod.GET)
-        private String testSsm(Model model, HttpServletRequest request){
-            int userId=Integer.parseInt(request.getParameter("userId"));
-            System.out.println("userId: 等于"+userId);
-            User user=null;
-            if(userId==1){
-                user=new User();
-                user.setAge(20);
-                user.setPassword("123456");
-                user.setUsername("钟桑扬");
-            }
-            logger.debug(user.toString());
-            model.addAttribute("user",user);
-            return "index";
+    // /user/test?id=1
+    @GetMapping(value = "/test")
+    private String testSsm(Model model, HttpServletRequest request) {
+        int userId = Integer.parseInt(request.getParameter("userId"));
+        System.out.println("userId: 等于" + userId);
+        User user = null;
+        if (userId == 1) {
+            user = new User();
+            user.setAge(20);
+            user.setPassword("123456");
+            user.setUsername("钟桑扬");
         }
+        logger.debug(user.toString());
+        model.addAttribute("user", user);
+        return "index";
+    }
 }
